d5fe023e7b8b9095d1dd3c4be8e03b697a0d5647
Vianny Reyes <reyesvi92@gmail.com>
2020-03-19 12:03:37 -0300

    map title con react-leaflet mostrando mapa y ubicacion con navigator.geolocation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import MapContainer from "./MapContainer";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: -33.43778,
+      lng: -70.65028
+    };
+  }
+  render() {
+    const center = [this.state.lat, this.state.lng];
+    const zoom = 50;
+    return <MapContainer center={center} zoom={zoom} />;
+  }
+  componentDidMount() {
+    
+  }
+
+  onSuccessPosition(pos) {
+    
+    this.setState({ ...this.state, lat: crd.latitude, lng: crd.longitude });
+  }
 }
 
 export default App;
